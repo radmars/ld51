21022da3925f7ada6cbd47016ab7ad6b9a78da1e
Trevor Williams <whiltx@gmail.com>
2022-10-01 17:05:43 -0700

    Migrate to variables from properties so I don't accidentally clobber existing ones.

--- src/Game.js
+++ src/Game.js
@@ -8,6 +8,13 @@ const Global = {
 };
 
 let ticking = false;
+let cursors;
+let player;
+let freezeBullets;
+let ticker;
+let countdownText;
+let freezeLastFired = 0;
+let center;
 
 function tenSecondTick() {
     ticking = true;
@@ -19,15 +26,7 @@ class Main extends Phaser.Scene
     {
         super();
 
-        this.cursors;
-        this.player;
-        this.freezeBullets;
-        this.ticker;
-        this.countdownText;
-
-        this.freezeLastFired = 0;
-        this.center = {x: Global.width / 2, y: Global.height / 2};
-        this.rotateSpeed = Global.rotateSpeed;
+        center = {x: Global.width / 2, y: Global.height / 2};
     }
 
     preload ()
@@ -35,17 +34,45 @@ class Main extends Phaser.Scene
         this.load.image('player', 'assets/ship.png');
         this.load.image('center', 'assets/bomb.png');
         this.load.image('freezeBullet', 'assets/freezeBullet.png');
+        this.load.image('bacteria', 'assets/star.png');
     }
 
     create ()
     {
-        this.ticker = this.time.addEvent({
+        ticker = this.time.addEvent({
             delay: 10000,
             callback: tenSecondTick,
             callbackScope: this,
             loop: true,
         })
-        this.countdownText = this.add.text(0, 0, '10.0', { fill: '#00ff00' });
+
+        countdownText = this.add.text(0, 0, '10.0', { fill: '#00ff00' });
+
+        var Bacteria = new Phaser.Class({
+            Extends: Phaser.GameObjects.Image,
+
+            initialize:
+
+            function Bacteria (scene)
+            {
+                Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bacteria');
+            },
+
+            update: function (time, delta)
+            {
+                this.x += this.speed * delta * Math.cos(this.rotation);
+                this.y += this.speed * delta * Math.sin(this.rotation);
+
+                // Pull toward the center
+
+                // TODO: Use boundary collisions instead, or maybe just timed life.
+                if (this.y < 0 || this.y > Global.height || this.x < 0 || this.x > Global.width)
+                {
+                    this.setActive(false);
+                    this.setVisible(false);
+                }
+            }
+        })
 
         var FreezeBullet = new Phaser.Class({
             Extends: Phaser.GameObjects.Image,
@@ -82,39 +109,39 @@ class Main extends Phaser.Scene
             },
         });
 
-        this.freezeBullets = this.add.group({
+        freezeBullets = this.add.group({
             classType: FreezeBullet,
             maxSize: 5,
             runChildUpdate: true
         });
 
-        this.add.image(this.center.x, this.center.y, 'center').setDepth(1);
-        this.player = this.add.image(400, 50, 'player').setDepth(1);
+        this.add.image(center.x, center.y, 'center').setDepth(1);
+        player = this.add.image(400, 50, 'player').setDepth(1);
 
-        this.cursors = this.input.keyboard.createCursorKeys();
+        cursors = this.input.keyboard.createCursorKeys();
     }
 
     update (time, delta)
     {
         // Player rotates around center.
-        Phaser.Actions.RotateAroundDistance([this.player], this.center, this.rotateSpeed, 250);
-        const angleDeg = Math.atan2(this.player.y - this.center.y, this.player.x - this.center.x) * 180 / Math.PI;
-        this.player.angle = angleDeg-90; // container should face the center point
+        Phaser.Actions.RotateAroundDistance([player], center, Global.rotateSpeed, 250);
+        const angleDeg = Math.atan2(player.y - center.y, player.x - center.x) * 180 / Math.PI;
+        player.angle = angleDeg-90; // container should face the center point
 
         // Fire projectile on "up".
-        if (this.cursors.up.isDown && time > this.freezeLastFired + Global.freezeCooldown)
+        if (cursors.up.isDown && time > freezeLastFired + Global.freezeCooldown)
         {
-            let bullet = this.freezeBullets.get();
+            let bullet = freezeBullets.get();
 
             if (bullet)
             {
-                let angle = Phaser.Math.Angle.BetweenPoints(this.player, this.center)
-                bullet.fire(this.player.x, this.player.y, angle);
-                this.freezeLastFired = time;
+                let angle = Phaser.Math.Angle.BetweenPoints(player, center)
+                bullet.fire(player.x, player.y, angle);
+                freezeLastFired = time;
             }
         }
 
-        this.countdownText.setText(this.ticker.getRemainingSeconds().toString().substr(0, 4));
+        countdownText.setText(ticker.getRemainingSeconds().toString().substr(0, 4));
 
         ticking = false;
     }

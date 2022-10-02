92377fce31258a9d3acefb545800517738d5e537
Trevor Williams <whiltx@gmail.com>
2022-10-01 19:02:03 -0700

    Bacteria orbit the center.

--- src/Game.js
+++ src/Game.js
@@ -7,14 +7,19 @@ const Global = {
     freezeCooldown: 200,
 };
 
+const center = {
+    x: Global.width / 2,
+    y: Global.height / 2,
+}
+
 let ticking = false;
 let cursors;
 let player;
 let freezeBullets;
+let bacteria;
 let ticker;
 let countdownText;
 let freezeLastFired = 0;
-let center;
 
 function tenSecondTick() {
     ticking = true;
@@ -25,14 +30,11 @@ class Main extends Phaser.Scene
     constructor ()
     {
         super();
-
-        center = {x: Global.width / 2, y: Global.height / 2};
     }
 
     preload ()
     {
         this.load.image('player', 'assets/ship.png');
-        this.load.image('center', 'assets/bomb.png');
         this.load.image('freezeBullet', 'assets/freezeBullet.png');
         this.load.image('bacteria', 'assets/star.png');
     }
@@ -56,14 +58,18 @@ class Main extends Phaser.Scene
             function Bacteria (scene)
             {
                 Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bacteria');
+
+                this.velX = Phaser.Math.FloatBetween(-1, 1);
+                this.velY = Phaser.Math.FloatBetween(-1, 1);
             },
 
             update: function (time, delta)
             {
-                this.x += this.speed * delta * Math.cos(this.rotation);
-                this.y += this.speed * delta * Math.sin(this.rotation);
-
                 // Pull toward the center
+                this.velX += (center.x - this.x) * 0.0001;
+                this.velY += (center.y - this.y) * 0.0001;
+                this.x += this.velX;
+                this.y += this.velY;
 
                 // TODO: Use boundary collisions instead, or maybe just timed life.
                 if (this.y < 0 || this.y > Global.height || this.x < 0 || this.x > Global.width)
@@ -71,7 +77,12 @@ class Main extends Phaser.Scene
                     this.setActive(false);
                     this.setVisible(false);
                 }
-            }
+            },
+
+            split: function()
+            {
+                // TODO
+            },
         })
 
         var FreezeBullet = new Phaser.Class({
@@ -83,7 +94,7 @@ class Main extends Phaser.Scene
             {
                 Phaser.GameObjects.Image.call(this, scene, 0, 0, 'freezeBullet');
 
-                this.speed = Phaser.Math.GetSpeed(400, 1);
+                this.speed = Phaser.Math.GetSpeed(600, 1);
             },
 
             fire: function (x, y, angle)
@@ -109,16 +120,34 @@ class Main extends Phaser.Scene
             },
         });
 
-        freezeBullets = this.add.group({
+        freezeBullets = this.physics.add.group({
             classType: FreezeBullet,
             maxSize: 5,
-            runChildUpdate: true
+            runChildUpdate: true,
         });
 
-        this.add.image(center.x, center.y, 'center').setDepth(1);
-        player = this.add.image(400, 50, 'player').setDepth(1);
+        bacteria = this.physics.add.group({
+            classType: Bacteria,
+            maxSize: 50,
+            runChildUpdate: true,
+        })
+        bacteria.createMultiple({
+            key: 'bacteria', // Not sure why I need to specify this
+            quantity: 5,
+            active: true,
+            visible: true,
+        });
+
+        // Put bacteria randomly within a central circle
+        const circle = new Phaser.Geom.Circle(Global.width / 2, Global.height / 2, 150);
+        Phaser.Actions.RandomCircle(bacteria.getChildren(), circle);
+
+        player = this.physics.add.image(400, 50, 'player')
+            .setDepth(1);
 
         cursors = this.input.keyboard.createCursorKeys();
+
+        // this.physics.add.collider(freezeBullets.getChildren(), bacteria.getChildren());
     }
 
     update (time, delta)
@@ -126,7 +155,7 @@ class Main extends Phaser.Scene
         // Player rotates around center.
         Phaser.Actions.RotateAroundDistance([player], center, Global.rotateSpeed, 250);
         const angleDeg = Math.atan2(player.y - center.y, player.x - center.x) * 180 / Math.PI;
-        player.angle = angleDeg-90; // container should face the center point
+        player.angle = angleDeg - 90; // should face the center point.
 
         // Fire projectile on "up".
         if (cursors.up.isDown && time > freezeLastFired + Global.freezeCooldown)
@@ -152,6 +181,12 @@ const config = {
   width: Global.width,
   height: Global.height,
   scene: [ Main ],
+  physics: {
+    default: 'arcade',
+    arcade: {
+        debug: true,
+    },
+  },
 };
 
 const game = new Phaser.Game(config);
